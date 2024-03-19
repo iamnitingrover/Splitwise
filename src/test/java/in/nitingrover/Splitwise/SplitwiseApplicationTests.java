70755913e7ba0b978ad01a1966dfe0744ef40d77
Nitin Grover <nitingrover@live.com>
2024-03-19 20:46:21 +0530

    Initial Commit

---
+++ src/test/java/in/nitingrover/Splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nitingrover.Splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
